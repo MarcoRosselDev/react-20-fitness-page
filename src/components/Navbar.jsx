ee0966129c79e64d34494477135190f8b3b73d43
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-29 23:02:03 -0300

    navbar passing links map data.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,25 @@
 import { Link, NavLink } from "react-router-dom";
-
-import "./navbar.css";
 import Logo from "../images/logo.png";
+import { links } from "../../data";
+import "./navbar.css";
 
 function Navbar() {
   return (
     <nav>
-      <div className="container nav_container">
+      <div className="container nav__container">
         <Link to="/" className="logo">
           <img src={Logo} alt="Nav Logo" />
         </Link>
-        nav links nav buttons
+        <ul className="nav__links">
+          {links.map(({ name, path }, index) => {
+            return (
+              <li>
+                <NavLink to={path}>{name}</NavLink>
+              </li>
+            );
+          })}
+        </ul>
+        nav buttons
       </div>
     </nav>
   );
