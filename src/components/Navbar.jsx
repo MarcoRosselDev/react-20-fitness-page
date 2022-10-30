1c8bd0cab59da08e48a6f5fbd7f7e9e86d8df10e
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-29 22:34:01 -0300

    navbar components 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import "./navbar.css";
+import Logo from "../images/logo.png";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div className="container nav_container">logo nav links nav buttons</div>
+    </nav>
+  );
 }
 
 export default Navbar;
