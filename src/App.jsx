a7775d8b998e7324897ed49a17507650d7393373
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-29 22:23:38 -0300

    import all pages on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import Home from "./pages/home/Home";
+import Contact from "./pages/contact/Contact";
+import Gallery from "./pages/gallery/Gallery";
+import About from "./pages/about/About";
+import NotFound from "./pages/notFound/NotFound";
+import Plans from "./pages/plans/Plans";
+import Trainers from "./pages/trainers/Trainers";
+
 const App = () => {
-  return <div>React website</div>;
+  return (
+    <div>
+      <Home />
+      <Contact />
+      <Gallery />
+      <About />
+      <NotFound />
+      <Plans />
+      <Trainers />
+    </div>
+  );
 };
 
 export default App;
