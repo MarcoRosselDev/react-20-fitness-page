6ff44fd00ba2af19a058684b2b673bfc2cfdeca4
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-30 23:47:12 -0300

    media screen styles toggle successfull |

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
         >
           {links.map(({ name, path }, index) => {
             return (
-              <li>
+              <li key={index}>
                 <NavLink
                   to={path}
                   className={({ isActive }) => (isActive ? "active-nav" : "")}
