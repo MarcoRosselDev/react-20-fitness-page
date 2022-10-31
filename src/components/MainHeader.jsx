e2bbb1d7fd90e6cd73538c9d6a65f03adc317e68
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-31 18:18:42 -0300

    mainHeader.jsx main-header-left

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -2,7 +2,16 @@ function MainHeader() {
   return (
     <header className="main__header">
       <div className="container main__header-container">
-        <div className="main__header-left"></div>
+        <div className="main__header-left">
+          <h4>#100DaysOfWorkOut</h4>
+          <h1>Join The Legends Of The Fitness World</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+            magnam. Molestias facilis dolorem aspernatur est laudantium ut
+            repudiandae? Placeat nam animi esse ipsa aliquid aspernatur
+            assumenda fugit recusandae quasi sint.
+          </p>
+        </div>
         <div className="main__header-right"></div>
       </div>
     </header>
