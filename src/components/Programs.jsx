1785740b977cb6de891601b36bc5c6d0f7ecf73c
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-31 22:49:07 -0300

    Programs.jsx last move

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -1,11 +1,17 @@
 import { FaCrown } from "react-icons/fa";
 import SectionHead from "./SectionHead";
+import { programs } from "../data";
 
 function Programs() {
   return (
     <section className="programs">
       <div className="container programs__container">
         <SectionHead icon={<FaCrown />} title="Programs" />
+      </div>
+      <div className="programs__wrapper">
+        {programs.map(({ id, icon, title, info, path }) => {
+          return <h4 key={id}>{title}</h4>;
+        })}
       </div>
     </section>
   );
