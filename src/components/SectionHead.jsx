20a952ba1dfdf3d3eba7c5f1338bd9204fe42709
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-10-31 22:34:37 -0300

    SectionHead.jsx component

---
+++ src/components/SectionHead.jsx
@@ -0,0 +1,10 @@
+const SectionHead = ({ icon, title, className }) => {
+  return (
+    <div className="programs__head">
+      <span>{<FaCrown />}</span>
+      <h2>Programs</h2>
+    </div>
+  );
+};
+
+export default SectionHead;
