669d4de01147ce2a29b3a7fa21464c7113fe36c2
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-01 23:58:45 -0300

    FAQ.jsx structure solved two errors console

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -7,7 +7,7 @@ function FAQs() {
   return (
     <section className="faqs">
       <div className="container faqs__container">
-        <SectionHead icon={FaQuestion} title="FAQs" />
+        <SectionHead icon={<FaQuestion />} title="FAQs" />
         <div className="faqs__wrapper">
           {faqs.map(({ id, question, answer }) => {
             return <FAQ key={id} question={question} answer={answer} />;
