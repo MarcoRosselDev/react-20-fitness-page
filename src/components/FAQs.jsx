4bd583ece62e7f159e5030e7ba7e65e7e1d4eda4
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-01 23:45:02 -0300

    FAQs.jsx structure

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -1,9 +1,17 @@
 import SectionHead from "./SectionHead";
 import { FaQuestion } from "react-icons/fa";
 import { faqs } from "../data";
+import FAQ from "./FAQ";
 
 function FAQs() {
-  return <div>FAQs</div>;
+  return (
+    <section className="faqs">
+      <div className="container faqs__container">
+        <SectionHead icon={FaQuestion} title="FAQs" />
+        <div className="faqs__wrapper">{}</div>
+      </div>
+    </section>
+  );
 }
 
 export default FAQs;
