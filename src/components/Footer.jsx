2232f17e5e6f6f06aba8dd9d47e99b623f8062a6
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-02 16:28:04 -0300

    Footer.jsx component structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,56 @@
+import { Link } from "react-router-dom";
+import Logo from "../images/logo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container footer__container">
+        <article>
+          <Link to="/" className="logo">
+            <img src={Logo} alt="Footer logo" />
+          </Link>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+            numquam sint rerum sequi nulla ipsam sapiente minima nostrum non.
+          </p>
+          <div className="footer__socials">
+            <a
+              href="http://linkedin.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="http://facebook.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaFacebook />
+            </a>
+            <a
+              href="http://twitter.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <AiOutlineTwitter />
+            </a>
+            <a
+              href="http://instagram.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <AiFillInstagram />
+            </a>
+          </div>
+        </article>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
