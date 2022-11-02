bddd5982376580b7b95f96191e2cd193431fc712
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-01 23:40:06 -0300

    FAQs.jsx

---
+++ src/components/FAQs.jsx
@@ -0,0 +1,9 @@
+import SectionHead from "./SectionHead";
+import { FaQuestion } from "react-icons/fa";
+import { faqs } from "../data";
+
+function FAQs() {
+  return <div>FAQs</div>;
+}
+
+export default FAQs;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import MainHeader from "../../components/MainHeader";
 import Programs from "../../components/Programs";
 import Values from "../../components/Values";
+import FAQs from "../../components/FAQs";
 import "./home.css";
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
       <MainHeader />
       <Programs />
       <Values />
+      <FAQs />
     </>
   );
 }
