f2c803d10736c0bf7045f52ceab84be35fbf63df
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-02 23:17:15 -0300

    Header component structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
-function Header() {
-  return <div>Header</div>;
-}
+const Header = ({ title, image, children }) => {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <div className="header__container-bg">
+          <img src={image} alt="Header Background Image" />
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
