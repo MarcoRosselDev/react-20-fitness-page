d03ca668e583be78e8f1077036302f1941e084b0
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-02 23:20:44 -0300

    Header component structure 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,10 @@ const Header = ({ title, image, children }) => {
         <div className="header__container-bg">
           <img src={image} alt="Header Background Image" />
         </div>
+        <div className="header__content">
+          <h2>{title}</h2>
+          <p>{children}</p>
+        </div>
       </div>
     </header>
   );
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,13 @@
+import Header from "../../components/Header";
 import "./about.css";
+import HeaderImage from "../../images/header_bg_1.jpg";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <Header title="About Us" image={HeaderImage}></Header>
+    </>
+  );
 };
 
 export default About;
