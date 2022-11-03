005754fef1405612daf4437c4515742ce0115a3f
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-02 23:30:11 -0300

    Header component styles

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -5,7 +5,11 @@ import HeaderImage from "../../images/header_bg_1.jpg";
 const About = () => {
   return (
     <>
-      <Header title="About Us" image={HeaderImage}></Header>
+      <Header title="About Us" image={HeaderImage}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo facilis a
+        ipsum distinctio laboriosam accusantium assumenda quia iusto eius
+        consequatur.
+      </Header>
     </>
   );
 };
