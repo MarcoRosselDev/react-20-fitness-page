c3cfbccdb0f151223e93d16c19125d56e4de47e8
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-03 23:45:04 -0300

    contact page structure

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,16 @@
 import "./contact.css";
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_2.jpg";
+import { MdEmail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
 
-function Contact() {
-  return <div>Contact</div>;
-}
+const Contact = () => {
+  return (
+    <>
+      <Header title="Get In Touch" image={HeaderImage}></Header>
+    </>
+  );
+};
 
 export default Contact;
