f1daf61d9d97e7246984625f4f2e589819a15951
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-03 23:55:25 -0300

    contact page socialmedia

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -8,7 +8,38 @@ import { IoLogoWhatsapp } from "react-icons/io";
 const Contact = () => {
   return (
     <>
-      <Header title="Get In Touch" image={HeaderImage}></Header>
+      <Header title="Get In Touch" image={HeaderImage}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam fugiat
+        similique architecto rerum facilis adipisci nobis possimus quaerat,
+        maiores nesciunt?
+      </Header>
+      <section className="contact">
+        <div className="container contact__container">
+          <div className="contact__wrapper">
+            <a
+              href="http://gmail.com"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <MdEmail />
+            </a>
+            <a
+              href="http://wa.me/+56951929740"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <IoLogoWhatsapp />
+            </a>
+            <a
+              href="http://m.me/ernest_achiever"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <BsMessenger />
+            </a>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
