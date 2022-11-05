a318fb6b5b0cef061ea3428f42df5e82e8e957e9
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-05 00:35:55 -0300

    gallery page

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,7 +1,16 @@
 import "./gallery.css";
+import HeaderImage from "../../images/header_bg_3.jpg";
+import Header from "../../components/Header";
 
 function Gallery() {
-  return <div>gallery</div>;
+  return (
+    <>
+      <Header title="Our Gallery" image={HeaderImage}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore fugiat
+        nihil fugit nemo et! Facere, id impedit.
+      </Header>
+    </>
+  );
 }
 
 export default Gallery;
