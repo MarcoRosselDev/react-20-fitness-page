ba8d5ed9a8ef30973f405730aa43166b82ec5a99
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-05 02:10:24 -0300

    trainers component

---
+++ src/components/Trainers.jsx
@@ -0,0 +1,25 @@
+import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
+import Card from "../UI/Card";
+
+const Trainers = ({ image, name, job, socials }) => {
+  return (
+    <Card className="trainer">
+      <div className="trainer__img">
+        <img src={image} alt={name} />
+      </div>
+      <h3>{name}</h3>
+      <p>{job}</p>
+      <div className="trainer__socials">
+        {socials.map(({ icon, link }, index) => {
+          return (
+            <a href={link} key={index} target="_blank" rel="noreffer noopener">
+              {icon}
+            </a>
+          );
+        })}
+      </div>
+    </Card>
+  );
+};
+
+export default Trainers;
