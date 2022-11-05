5df1d3a781080cf64cf463d7ddb97de2e0b86a57
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-05 02:02:29 -0300

    trainers page structure

--- src/pages/trainers/Trainers.jsx
+++ src/pages/trainers/Trainers.jsx
@@ -1,7 +1,25 @@
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_5.jpg";
+import { trainers } from "../../data";
+import { BsInstagram } from "react-icons/bi";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 import "./trainers.css";
 
-function Trainers() {
-  return <div>Trainers</div>;
-}
+const Trainers = () => {
+  return (
+    <>
+      <Header title="Our Trainers" image={HeaderImage}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
+        magnam doloribus itaque aspernatur velit.
+      </Header>
+      <section className="trainers">
+        <div className="cantainer trainers__container">
+          {trainers.map(({ id, image, name, job, socials }) => {})}
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Trainers;
